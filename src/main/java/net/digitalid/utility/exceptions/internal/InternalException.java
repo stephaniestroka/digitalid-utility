5df59c5e7aa3f7ffcdd7a58ca39d1517a37d7467
Stephanie Stroka <steffueue@gmail.com>
2016-01-05 23:36:54 +0100

    Continued refactoring the format converters. Format and converters are now split. Added contract annotations and final modifiers wherever applicable.

--- src/main/java/net/digitalid/utility/exceptions/internal/InternalException.java
+++ src/main/java/net/digitalid/utility/exceptions/internal/InternalException.java
@@ -21,6 +21,41 @@ public class InternalException extends DigitalIDException {
     protected InternalException(@Nonnull String message) {
         super(message);
     }
+
+    /**
+     * Creates a new internal exception with the given message and the given exception.
+     * 
+     * @param message a string explaining the illegal operation.
+     * @param exception an exception which is the cause of this exception.
+     */
+    protected InternalException(@Nonnull String message, @Nonnull Exception exception) {
+        super(message, exception);
+    }
+
+    /**
+     * Returns a new internal exception with the given exception.
+     *
+     * @param exception a cause of this exception.
+     *
+     * @return a new internal exception with the given exception.
+     */
+    @Pure
+    public static @Nonnull InternalException get(@Nonnull Exception exception) {
+        return new InternalException(exception.getMessage(), exception);
+    }
+    
+    /**
+     * Returns a new internal exception with the given message and the given exception.
+     *
+     * @param message a string explaining the illegal operation.
+     * @param exception a cause of this exception.
+     *
+     * @return a new internal exception with the given message and the given exception.
+     */
+    @Pure
+    public static @Nonnull InternalException get(@Nonnull String message, @Nonnull Exception exception) {
+        return new InternalException(message);
+    }
     
     /**
      * Returns a new internal exception with the given message.
