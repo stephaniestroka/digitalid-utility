79c6c1ad762dbebce70f463e569f984a632903a7
Kaspar Etter <kaspar.etter@digitalid.net>
2016-02-02 13:18:22 +0100

    Added annotations to describe the kind and nesting of a type.

---
+++ validation/src/main/java/net/digitalid/utility/validation/annotations/type/kind/AnnotationType.java
@@ -0,0 +1,36 @@
+package net.digitalid.utility.validation.annotations.type.kind;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.lang.model.element.Element;
+
+import net.digitalid.utility.validation.annotations.meta.TargetTypes;
+import net.digitalid.utility.validation.annotations.meta.ValidateWith;
+import net.digitalid.utility.validation.validator.AnnotationValidator;
+
+/**
+ * This annotation indicates that a class or element represents an annotation.
+ * 
+ * @see Class
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@ValidateWith(AnnotationType.Validator.class)
+@TargetTypes({Class.class, Element.class})
+@Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE, ElementType.METHOD})
+public @interface AnnotationType {
+    
+    /* -------------------------------------------------- Validator -------------------------------------------------- */
+    
+    /**
+     * This class checks the use of and generates the contract for the surrounding annotation.
+     */
+    public static class Validator extends AnnotationValidator {
+        // TODO: Generate the contract.
+    }
+    
+}
---
+++ validation/src/main/java/net/digitalid/utility/validation/annotations/type/nesting/MemberType.java
@@ -0,0 +1,37 @@
+package net.digitalid.utility.validation.annotations.type.nesting;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.lang.model.element.Element;
+import javax.lang.model.element.NestingKind;
+
+import net.digitalid.utility.validation.annotations.meta.TargetTypes;
+import net.digitalid.utility.validation.annotations.meta.ValidateWith;
+import net.digitalid.utility.validation.validator.AnnotationValidator;
+
+/**
+ * This annotation indicates that a class or element represents a member type.
+ * 
+ * @see NestingKind
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@ValidateWith(MemberType.Validator.class)
+@TargetTypes({Class.class, Element.class})
+@Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE, ElementType.METHOD})
+public @interface MemberType {
+    
+    /* -------------------------------------------------- Validator -------------------------------------------------- */
+    
+    /**
+     * This class checks the use of and generates the contract for the surrounding annotation.
+     */
+    public static class Validator extends AnnotationValidator {
+        // TODO: Generate the contract.
+    }
+    
+}
