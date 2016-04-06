ea8c988bcd56dc0ab8e4cc2335f0987dfd1e82d9
Stephanie Stroka <stephanie.stroka@digitalid.net>
2016-04-06 09:10:32 +0200

    Fixed package name of collaboration classes.

--- collaboration/src/main/java/net/digitalid/utility/collaboration/annotations/TODO.java
+++ collaboration/src/main/java/net/digitalid/utility/collaboration/annotations/TODO.java
@@ -8,8 +8,8 @@ import java.lang.annotation.Target;
 
 import javax.annotation.Nonnull;
 
-import net.digitalid.collaboration.enumerations.Author;
-import net.digitalid.collaboration.enumerations.Priority;
+import net.digitalid.utility.collaboration.enumerations.Author;
+import net.digitalid.utility.collaboration.enumerations.Priority;
 
 /**
  * This annotation indicates a task that still needs to be done.
