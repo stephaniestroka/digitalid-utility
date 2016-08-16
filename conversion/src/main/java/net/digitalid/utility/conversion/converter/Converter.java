9ce40c43df2b4a00d335dad8853ead181612b86e
Kaspar Etter <kaspar.etter@digitalid.net>
2016-08-16 11:24:36 +0200

    Added the J2ObjC Maven Plugin to the parent POM, which requires a ${J2OBJC_HOME} property and is executed with 'mvn j2objc:convert'.

--- conversion/src/main/java/net/digitalid/utility/conversion/converter/Converter.java
+++ conversion/src/main/java/net/digitalid/utility/conversion/converter/Converter.java
@@ -8,28 +8,32 @@ import net.digitalid.utility.annotations.ownership.Capturable;
 import net.digitalid.utility.annotations.ownership.NonCaptured;
 import net.digitalid.utility.annotations.parameter.Modified;
 import net.digitalid.utility.annotations.parameter.Unmodified;
+import net.digitalid.utility.collaboration.annotations.TODO;
+import net.digitalid.utility.collaboration.enumerations.Author;
 import net.digitalid.utility.immutable.ImmutableList;
+import net.digitalid.utility.validation.annotations.size.MaxSize;
 import net.digitalid.utility.validation.annotations.string.CodeIdentifier;
 import net.digitalid.utility.validation.auxiliary.None;
 
 public interface Converter<T, E> {
     
     @Pure
-    public @Nonnull ImmutableList<CustomField> getFields();
+    public @Nonnull ImmutableList<@Nonnull CustomField> getFields();
     
     @Pure
     public int convert(@Nullable @NonCaptured @Unmodified T object, @Nonnull @NonCaptured @Modified ValueCollector valueCollector);
     
     @Pure
+    @TODO(task = "Add a checked exception? If yes, an ExternalException?", date = "2016-08-08", author = Author.KASPAR_ETTER)
     public @Nonnull @Capturable T recover(@Nonnull @NonCaptured @Modified SelectionResult selectionResult, E externallyProvided);
     
     @Pure
-    public @Nonnull @CodeIdentifier String getName();
+    public @Nonnull @CodeIdentifier @MaxSize(63) String getName();
     
     public class NoneConverter implements Converter<None, Object> {
         
         public static @Nonnull NoneConverter INSTANCE = new NoneConverter();
-    
+        
         @Override
         public @Nonnull ImmutableList<@Nonnull CustomField> getFields() {
             return ImmutableList.withElements();
