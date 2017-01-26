343305c40aea131b6ea43781f4e098e36783c311
Kaspar Etter <kaspar.etter@digitalid.net>
2017-01-26 21:11:48 +0100

    Fixed the target for annotations that were restricted to only type use.

--- annotations/src/main/java/net/digitalid/utility/annotations/state/Modifiable.java
+++ annotations/src/main/java/net/digitalid/utility/annotations/state/Modifiable.java
@@ -15,6 +15,6 @@ import net.digitalid.utility.annotations.method.Impure;
  * @see Unmodifiable
  */
 @Documented
-@Target(ElementType.TYPE_USE)
+@Target({ElementType.TYPE_USE, ElementType.PARAMETER, ElementType.METHOD, ElementType.FIELD, ElementType.LOCAL_VARIABLE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Modifiable {}
--- annotations/src/main/java/net/digitalid/utility/annotations/state/Unmodifiable.java
+++ annotations/src/main/java/net/digitalid/utility/annotations/state/Unmodifiable.java
@@ -23,6 +23,6 @@ import net.digitalid.utility.annotations.method.Pure;
  * @see Modifiable
  */
 @Documented
-@Target(ElementType.TYPE_USE)
+@Target({ElementType.TYPE_USE, ElementType.PARAMETER, ElementType.METHOD, ElementType.FIELD, ElementType.LOCAL_VARIABLE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Unmodifiable {}
