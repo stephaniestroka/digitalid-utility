473a67cd88426bfa5a626e043e9164521e52ee5f
Kaspar Etter <kaspar.etter@digitalid.net>
2017-03-16 09:22:40 +0100

    Changed how the toString() method is generated.

--- generator/src/main/java/net/digitalid/utility/generator/typevisitors/GenerateToStringTypeVisitor.java
+++ generator/src/main/java/net/digitalid/utility/generator/typevisitors/GenerateToStringTypeVisitor.java
@@ -6,55 +6,41 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.lang.model.type.ArrayType;
 import javax.lang.model.type.DeclaredType;
-import javax.lang.model.type.PrimitiveType;
 import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.SimpleTypeVisitor7;
 
 import net.digitalid.utility.annotations.method.Pure;
 import net.digitalid.utility.circumfixes.Quotes;
-import net.digitalid.utility.contracts.Require;
 import net.digitalid.utility.processing.utility.ProcessingUtility;
 import net.digitalid.utility.processor.generator.JavaFileGenerator;
 import net.digitalid.utility.tuples.Pair;
+import net.digitalid.utility.validation.annotations.type.Stateless;
 
 /**
  * The type visitor prints character sequence values in double quotes and primitive type values without quotes.
  */
-public class GenerateToStringTypeVisitor extends SimpleTypeVisitor7<@Nonnull String, @Nullable Pair<String, JavaFileGenerator>> {
+@Stateless
+public class GenerateToStringTypeVisitor extends SimpleTypeVisitor7<@Nonnull String, @Nullable Pair<@Nonnull String, @Nonnull JavaFileGenerator>> {
     
     @Pure
     @Override
-    protected @Nonnull String defaultAction(@Nonnull TypeMirror type, @Nullable Pair<String, JavaFileGenerator> pair) {
-        Require.that(pair.get0() != null).orThrow("The field access code is a required parameter and cannot be generated on the fly. Please call visit(TypeMirror, String) instead.");
-        
-        return pair.get1().importIfPossible(Quotes.class) + ".inDouble(" + pair.get0() + ")";
-    }
-    
-    @Pure
-    @Override 
-    public @Nonnull String visitPrimitive(@Nonnull PrimitiveType type, @Nullable Pair<String, JavaFileGenerator> pair) {
-        Require.that(pair.get0() != null).orThrow("The field access code is a required parameter and cannot be generated on the fly. Please call visit(TypeMirror, String) instead.");
-        
+    protected @Nonnull String defaultAction(@Nonnull TypeMirror type, @Nullable Pair<@Nonnull String, @Nonnull JavaFileGenerator> pair) {
         return pair.get0();
     }
     
     @Pure
     @Override 
-    public @Nonnull String visitDeclared(@Nonnull DeclaredType type, @Nullable Pair<String, JavaFileGenerator> pair) {
-        Require.that(pair.get0() != null).orThrow("The field access code is a required parameter and cannot be generated on the fly. Please call visit(TypeMirror, String) instead.");
-        
+    public @Nonnull String visitDeclared(@Nonnull DeclaredType type, @Nullable Pair<@Nonnull String, @Nonnull JavaFileGenerator> pair) {
         if (ProcessingUtility.isRawSubtype(type, CharSequence.class)) {
             return pair.get1().importIfPossible(Quotes.class) + ".inDouble(" + pair.get0() + ")";
         } else {
-            return "String.valueOf(" + pair.get0() + ")";
+            return pair.get0();
         }
     }
     
     @Pure
     @Override 
-    public @Nonnull String visitArray(@Nonnull ArrayType type, @Nullable Pair<String, JavaFileGenerator> pair) {
-        Require.that(pair.get0() != null).orThrow("The field access code is a required parameter and cannot be generated on the fly. Please call visit(TypeMirror, String) instead.");
-        
+    public @Nonnull String visitArray(@Nonnull ArrayType type, @Nullable Pair<@Nonnull String, @Nonnull JavaFileGenerator> pair) {
         // TODO: Make sure that the elements are quoted if they are strings.
         return pair.get1().importIfPossible(Arrays.class) + ".toString(" + pair.get0() + ")";
     }
